8d8cf6ed54708a8972313317b328db6985d50b61
ravi4rails <ravionrails@gmail.com>
2019-06-21 00:32:08 +0530

    placed generator inside modules

--- lib/generators/bootstrap/install/install_generator.rb
+++ lib/generators/bootstrap/install/install_generator.rb
@@ -1,27 +1,32 @@
 require 'rails/generators'
 # This generator will add require assets to a rails application
-class InstallGenerator < ::Rails::Generators::Base
+module Bootstrap
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
 
-  def add_assets
-    js_manifest = 'app/assets/javascripts/application.js'
-    css_manifest = 'app/assets/stylesheets/application.css'
-    scss_manifest = 'app/assets/stylesheets/application.scss'
+      def add_assets
+        js_manifest = 'app/assets/javascripts/application.js'
+        css_manifest = 'app/assets/stylesheets/application.css'
+        scss_manifest = 'app/assets/stylesheets/application.scss'
 
-    if File.exist?(js_manifest)
-      insert_into_file js_manifest, "//= require jquery\n", after: "rails-ujs\n"
-      insert_into_file js_manifest, "//= require popper\n", after: "jquery\n"
-      insert_into_file js_manifest, "//= require bootstrap\n", after: "popper\n"
+        if File.exist?(js_manifest)
+          insert_into_file js_manifest, "//= require jquery\n", after: "rails-ujs\n"
+          insert_into_file js_manifest, "//= require popper\n", after: "jquery\n"
+          insert_into_file js_manifest, "//= require bootstrap\n", after: "popper\n"
+        end
+
+        if File.exist?(css_manifest)
+          style_require_block = " *= require bootstrap\n"
+          insert_into_file css_manifest, style_require_block, after: "require_self\n"
+        end
+
+        if File.exist?(scss_manifest)
+          insert_into_file scss_manifest, "@import 'bootstrap';\n"
+        end
+
+      end
+
     end
-
-    if File.exist?(css_manifest)
-      style_require_block = " *= require bootstrap\n"
-      insert_into_file css_manifest, style_require_block, after: "require_self\n"
-    end
-
-    if File.exist?(scss_manifest)
-      insert_into_file scss_manifest, "@import 'bootstrap';\n"
-    end
-
   end
-
 end
+
