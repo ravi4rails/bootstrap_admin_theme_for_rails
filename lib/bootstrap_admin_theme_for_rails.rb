d940f6ec4fadbbd0f10522b0146ba1f66c9a96a2
ravi4rails <ravionrails@gmail.com>
2019-06-20 13:48:28 +0530

    changed email into code conduct and added an error class

--- lib/bootstrap_admin_theme_for_rails.rb
+++ lib/bootstrap_admin_theme_for_rails.rb
@@ -1,6 +1,9 @@
 require "bootstrap_admin_theme_for_rails/version"
 
 module BootstrapAdminThemeForRails
+
+  class Error < StandardError; end
+
   def self.message
     puts "This is simple bootstrap admin theme for rails 5.x"
   end
